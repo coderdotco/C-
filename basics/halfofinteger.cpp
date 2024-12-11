4fd5cba0a98ef0e4499576130270f4428ea9c6f6
coderdotco <trupalmwd@gmail.com>
2024-12-11 16:02:26 +0530

    Add files via upload

---
+++ basics/halfofinteger.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"enter the number :";
+    cin>>x;
+    float y=(float)x; //typecasting conversion of int x to float x in new variable y
+    cout<<"the half of "<<x<<" is "<< y/2 <<endl ;
+    
+}
